168e61535c6555b55c9e0535b6c4bf5ca3652b1c
cl750 <clemensli123@gmail.com>
2026-09-17 12:07:46 -0400

    Beginning HTTPS GET and POST

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,11 +3,19 @@ from django.http import HttpRequest, HttpResponse
 import random, time
 # Create your views here.
 
+specials = ["Pralines & Cream", "Earl Grey Tea", "Hojicha", "Strawberry Shortcake", "Matcha", "Brown Sugar Boba", "Mango Sorbet"]
+
 def main_page(request):
     return render(request, "restaurant/main.html")
 
 def order_page(request):
-    return
+    context = {
+        "special": random.choice(specials)
+    }
+    return render(request, "restaurant/order.html", context)
 
-def confirmation_page(request):
-    return
+def show_confirmation_page(request):
+    return render(request, "confirmation.html")
+
+def submit_confirmation_page(request):
+    print(request)
