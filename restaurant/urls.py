8c3ffbdf09898b35dd9231a09e3085644d637e1d
cl750 <clemensli123@gmail.com>
2026-09-23 20:04:03 -0400

    Finishing up HTML, completed views and URLs and debugged the typos

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,3 +1,4 @@
+# URLS
 from django.urls import path
 from django.conf import settings
 from . import views
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,4 @@
+# Views
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 import random, time
@@ -6,16 +7,20 @@ import random, time
 specials = ["Pralines & Cream", "Earl Grey Tea", "Hojicha", "Strawberry Shortcake", "Matcha", "Brown Sugar Boba", "Mango Sorbet"]
 
 def main_page(request):
+    ''' Goes to the main page '''
     return render(request, "restaurant/main.html")
 
 def order_page(request):
+    ''' Goes to the order page with a random special '''
     context = {
-        "special": random.choice(specials)
+        "special": random.choice(specials) # Random daily item
     }
     return render(request, "restaurant/order.html", context)
 
 def show_confirmation_page(request):
+    ''' Confirmation request page '''
     return render(request, "confirmation.html")
 
 def submit_confirmation_page(request):
+    ''' Confirm submission '''
     print(request)
